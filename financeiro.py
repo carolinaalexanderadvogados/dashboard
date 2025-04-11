cde6b6bdfa432a3cec2f5000dff9e3ad58bb862d
carolinatakedaalexander <carolina.takeda@alexander.com.br>
2025-04-11 11:21:52 -0300

    Update financeiro.py

--- financeiro.py
+++ financeiro.py
@@ -59,7 +59,7 @@ def mostrar_financeiro():
     with graf1: 
         fig = go.Figure(go.Indicator(
             mode="gauge+number",
-            value=oxigenio_atual/10,
+            value=oxigenio_atual/100,
             title={"text": "Oxigênio em Meses"},
             gauge={
                 "axis": {"range": [0, oxigenio_objetivo]},
